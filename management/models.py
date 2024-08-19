3f21212bf21a5201bee693f808400abe15e0c19c
paulinenanzala <paulinenanzala19@gmail.com>
2024-08-19 21:45:24 +0300

    chore: update the requirement.txt file

--- management/models.py
+++ management/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
+from datetime import time
+
 
 class Bus(models.Model):
     bus_number = models.CharField(max_length=10, unique=True)
@@ -21,7 +22,7 @@ class Schedule(models.Model):
     bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     departure_time = models.TimeField()
-    arrival_time = models.TimeField()
+    arrival_time = models.TimeField(default=time(0, 0, 0))
 
     def __str__(self):
         return f"{self.bus} - {self.route} - {self.departure_time}"
